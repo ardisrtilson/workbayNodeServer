8d990f705f7c77d219a83332eba65659c0f249c0
ardisrtilson <email@example.com>
2021-04-25 13:24:32 -0500

    Added new search endpoint

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 var express = require('express');
+const axios = require('axios');
 var app = express();
 var fs = require("fs");
+const fetch = require("node-fetch");
+
+async function filterClasses(searchString){
+   try {
+     return getClasses().then(res => {
+     let classesArray = []
+     let flattenedClasses = [].concat.apply([], res.data)
+     for (let i = 0; i < flattenedClasses.length; i++) {
+         for (let j = 0; j < flattenedClasses[i].classificationId.length; j++) {
+             if (flattenedClasses[i].classificationType[j] === "SKILL" && flattenedClasses[i].classificationName[j].toUpperCase() === searchString.toUpperCase()){
+                 classesArray.push(flattenedClasses[i])
+             }
+     }
+ }
+ return classesArray
+}
+)} catch{console.log(error.response.body);}
+}
+
+async function getClasses() {
+   try {
+     const response = await axios.get('http://localhost:8000/classes')
+     return response
+   } catch (error) {
+     console.log(error.response.body);
+   }}
 
 app.get('/listClasses', function (req, res) {
    fs.readFile( __dirname + "/" + "classes.json", 'utf8', function (err, data) {
@@ -14,8 +41,17 @@ app.get('/uniqueSkills', function (req, res) {
    });
 })
 
+app.use('/', async (req, res, next) => {
+   try{const filters = req.query;
+   let classes = await filterClasses(filters.id).then(res=> res)
+   let prettyClasses = JSON.stringify(classes)
+   res.send(prettyClasses)
+   next()}
+   catch(error){console.log(error)}
+});
+
 // change port number to process.env.PORT when ready to deploy
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(5000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
